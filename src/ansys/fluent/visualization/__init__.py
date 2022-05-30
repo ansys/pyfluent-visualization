412741ddddaaac3a9e226538ece9be60dde96975
Mainak Kundu <mainak.kundu@ansys.com>
2022-05-30 16:57:55 +0530

    Creating pyfluent-visualization repo

---
+++ src/ansys/fluent/visualization/__init__.py
@@ -0,0 +1,12 @@
+"""
+fluent.
+
+visualization
+"""
+
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__.replace(".", "-"))
