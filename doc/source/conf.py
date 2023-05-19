86bdec59fba0695fc913b0777a00b5141b555bfd
Mainak Kundu <94432368+mkundu1@users.noreply.github.com>
2023-05-19 10:51:46 -0400

    Update to pyansys action v4 (#278)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -180,7 +180,7 @@ html_theme = "ansys_sphinx_theme"
 html_logo = pyansys_logo_black
 html_theme_options = {
     "switcher": {
-        "json_url": f"https://{cname}/release/versions.json",
+        "json_url": f"https://{cname}/versions.json",
         "version_match": get_version_match(__version__),
     },
     "navbar_end": ["version-switcher", "theme-switcher", "navbar-icon-links"],
