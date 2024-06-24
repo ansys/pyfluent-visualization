ae36f97fa1910f60f1ab649f912fa5d2732d9a1c
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2024-06-24 17:31:13 +0530

    Fix example.

--- examples/00-postprocessing/post_processing_exhaust_manifold.py
+++ examples/00-postprocessing/post_processing_exhaust_manifold.py
@@ -22,6 +22,8 @@ from ansys.fluent.visualization import set_config
 from ansys.fluent.visualization.matplotlib import Plots
 from ansys.fluent.visualization.pyvista import Graphics
 
+pyfluent.CONTAINER_MOUNT_PATH = pyfluent.EXAMPLES_PATH
+
 set_config(blocking=True, set_view_on_display="isometric")
 
 ###############################################################################
