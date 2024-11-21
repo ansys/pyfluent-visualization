71a6bc59427621dec4a68445a7cb956cb67b109b
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2024-11-21 13:44:30 +0530

    Updated example script.

--- examples/00-postprocessing/updated_script_manifold_example.py
+++ examples/00-postprocessing/updated_script_manifold_example.py
@@ -11,95 +11,114 @@ is an exhaust manifold.
 # Run the following in command prompt to execute this file:
 # exec(open("updated_script_manifold_example.py").read())
 
-import ansys.fluent.core as pyfluent
-
-session = pyfluent.connect_to_fluent(ip="10.18.44.105", port=61042, password="gcr7ienc")
-
 from ansys.fluent.visualization import set_config
 
 set_config(blocking=False)
 
+import ansys.fluent.core as pyfluent
+from ansys.fluent.core import examples
 
-from ansys.fluent.visualization import Graphics, Plots
-from ansys.fluent.visualization.graphics.graphics_windows import (
-    GraphicsWrapper as GraphicsWindow,
-)
-from ansys.fluent.visualization.plotter.plotter_windows import (
-    PlotterWrapper as PlotterWindow,
+import_case = examples.download_file(
+    file_name="exhaust_system.cas.h5", directory="pyfluent/exhaust_system"
 )
 
-# get the graphics objects for the session
-graphics_session = Graphics(session)
+import_data = examples.download_file(
+    file_name="exhaust_system.dat.h5", directory="pyfluent/exhaust_system"
+)
+
+session = pyfluent.launch_fluent(
+    precision="double",
+    processor_count=2,
+    start_transcript=False,
+    mode="solver",
+    ui_mode="gui",
+)
+
+session.settings.file.read_case(file_name=import_case)
+session.settings.file.read_data(file_name=import_data)
+
+from ansys.fluent.visualization import (
+    Contour,
+    GraphicsWindow,
+    Mesh,
+    Monitor,
+    Pathline,
+    PlotterWindow,
+    Surface,
+    Vector,
+    XYPlot,
+)
 
 # mesh
-mesh1 = graphics_session.Meshes.create(
-    show_edges=True, surfaces_list=["solid_up:1:830"]
-)
+mesh1 = Mesh(solver=session, show_edges=True, surfaces=["solid_up:1:830"])
 
 # pathlines
-pathlines1 = graphics_session.Pathlines.create(
-    field="velocity-magnitude", surfaces_list=["inlet"]
-)
+pathlines1 = Pathline(solver=session, field="velocity-magnitude", surfaces=["inlet"])
 
 # contour
-contour1 = graphics_session.Contours.create(
-    field="velocity-magnitude", surfaces_list=["solid_up:1:830"]
+contour1 = Contour(
+    solver=session, field="velocity-magnitude", surfaces=["solid_up:1:830"]
 )
 
 # vector
-vector1 = graphics_session.Vectors.create(
-    surfaces_list=["solid_up:1:830"], scale=4.0, skip=0, field="temperature"
+vector1 = Vector(
+    solver=session, surfaces=["solid_up:1:830"], scale=4.0, skip=0, field="temperature"
 )
 p_vect = GraphicsWindow()
-p_vect.plot(vector1)
+p_vect.add_graphics(vector1)
+p_vect.show()
 
 # iso surface
-surface1 = graphics_session.Surfaces.create()
+surface1 = Surface(solver=session)
 surface1.definition.type = "iso-surface"
 surface1.definition.iso_surface.field = "velocity-magnitude"
 surface1.definition.iso_surface.rendering = "contour"
 surface1.definition.iso_surface.iso_value = 0.0
 
 
-local_surfaces_provider = Graphics(session).Surfaces
-matplotlib_plots = Plots(session, local_surfaces_provider=local_surfaces_provider)
-
-
-p1 = matplotlib_plots.XYPlots.create()
-p1.surfaces_list = ["solid_up:1:830", "surface-0"]
-p1.surfaces_list = ["solid_up:1:830"]
+p1 = XYPlot(solver=session, surfaces=["solid_up:1:830"])
 p1.y_axis_function = "temperature"
-p1.plot("p1")
+p_xy = PlotterWindow()
+p_xy.add_plots(p1)
+p_xy.show()
 
 session.monitors.get_monitor_set_names()
-residual = matplotlib_plots.Monitors.create()
+residual = Monitor(solver=session)
 residual.monitor_set_name = "residual"
 p_res = PlotterWindow()
-p_res.plot(residual)
+p_res.add_plots(residual)
+p_res.show()
 
-mtr = matplotlib_plots.Monitors.create()
+mtr = Monitor(solver=session)
 mtr.monitor_set_name = "mass-tot-rplot"
 p_mtr = PlotterWindow()
-p_mtr.plot(mtr)
+p_mtr.add_plots(mtr)
+p_mtr.show()
 
-mbr = matplotlib_plots.Monitors.create()
+mbr = Monitor(solver=session)
 mbr.monitor_set_name = "mass-bal-rplot"
 p_mbr = PlotterWindow()
-p_mbr.plot(mbr)
+p_mbr.add_plots(mbr)
+p_mbr.show()
 
 p_mesh = GraphicsWindow()
-p_mesh.plot(mesh1)
+p_mesh.add_graphics(mesh1)
+p_mesh.show()
 p_vect = GraphicsWindow()
-p_vect.plot(vector1)
+p_vect.add_graphics(vector1)
+p_vect.show()
 p_cont = GraphicsWindow()
-p_cont.plot(contour1)
+p_cont.add_graphics(contour1)
+p_cont.show()
 p_pathline = GraphicsWindow()
-p_pathline.plot(pathlines1)
+p_pathline.add_graphics(pathlines1)
+p_pathline.show()
 
 p_cont.plotter.view_isometric()
 
 p_surf = GraphicsWindow()
-p_surf.plot(surface1)
+p_surf.add_graphics(surface1)
+p_surf.show()
 
 
 def auto_refersh_call_back_iteration(session_id, event_info):
