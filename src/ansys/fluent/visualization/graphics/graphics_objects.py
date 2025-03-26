0b892f54b9c33bc724611d68c1b350ed3f6cfa8c
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2025-03-26 16:32:37 +0530

    Move deps.

--- src/ansys/fluent/visualization/graphics/graphics_objects.py
+++ src/ansys/fluent/visualization/graphics/graphics_objects.py
@@ -25,16 +25,16 @@
 import sys
 from typing import Optional
 
-from ansys.fluent.core.post_objects.meta import Command
-from ansys.fluent.core.post_objects.post_helper import PostAPIHelper
-from ansys.fluent.core.post_objects.post_object_definitions import (
+from ansys.fluent.interface.meta import Command
+from ansys.fluent.interface.post_helper import PostAPIHelper
+from ansys.fluent.interface.post_object_definitions import (
     ContourDefn,
     MeshDefn,
     PathlinesDefn,
     SurfaceDefn,
     VectorDefn,
 )
-from ansys.fluent.core.post_objects.post_objects_container import (
+from ansys.fluent.interface.post_objects_container import (
     Graphics as GraphicsContainer,
 )
 
