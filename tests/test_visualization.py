0f7f7316b83099553c5287017bb1dc1436dcdab6
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2026-01-11 20:34:05 +0530

    Updates.

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -552,13 +552,13 @@ def test_get_raw_data_from_viz_object(new_solver_session_with_exhaust_case_and_d
         creation_method="xy-plane",
         z=-0.0441921,
     )
-    temp_key = surf_xy_plane.get_raw_data().keys()[0]
+    temp_key = list(surf_xy_plane.get_raw_data().keys())[0]
     assert surf_xy_plane.get_raw_data()[temp_key].vertices.shape == (15423, 3)
 
     surf_outlet_plane = Surface(solver=solver, type="iso-surface")
     surf_outlet_plane.field = "y-coordinate"
     surf_outlet_plane.iso_value = -0.125017
-    temp_key = surf_xy_plane.get_raw_data().keys()[0]
+    temp_key = list(surf_outlet_plane.get_raw_data().keys())[0]
     assert surf_outlet_plane.get_raw_data()[temp_key].connectivity.shape == (6556,)
 
     surf_vel_contour = Surface(
