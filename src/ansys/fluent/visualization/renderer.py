3673643ac6ab94d52623e99bf1e631e5cfea2ec2
James Hilton-Balfe <gobot1234yt@gmail.com>
2026-02-10 10:55:15 +0000

    Fix locals() usage in renderer.py

--- src/ansys/fluent/visualization/renderer.py
+++ src/ansys/fluent/visualization/renderer.py
@@ -89,8 +89,7 @@ class GraphicsWindow:
         """
         self._list_of_positions.append(position)
         if isinstance(graphics_obj._obj, GraphicsDefn):
-            locals()["object"] = locals().pop("graphics_obj")
-            self._graphics_objs.append({**locals()})
+            self._graphics_objs.append({"object": graphics_obj, "opacity": opacity, "position": position, "kwargs": kwargs})
         else:
             warnings.warn("Only graphics objects are supported.")
 
@@ -98,7 +97,6 @@ class GraphicsWindow:
         self,
         plot_obj,
         position: tuple = (0, 0),
-        title: str | None = None,
         **kwargs,
     ) -> None:
         """Add 2D plot-data to a window.
@@ -114,8 +112,8 @@ class GraphicsWindow:
         """
         self._list_of_positions.append(position)
         if isinstance(plot_obj._obj, PlotDefn):
-            locals()["object"] = locals().pop("plot_obj")
-            self._graphics_objs.append({**locals()})
+            self._graphics_objs.append({"object": plot_obj, "position": position, "kwargs": kwargs})
+        else:
         else:
             warnings.warn("Only 2D plot objects are supported.")
 
